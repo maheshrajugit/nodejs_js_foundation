35ca9cc934ce4bc7015977671aedbb09d7801b4c
pavan <pavan@wibesapp.com>
2023-09-28 20:24:46 +0530

    added attempts

--- Guess_number.js
+++ Guess_number.js
@@ -5,19 +5,31 @@ let guess = 0;
 console.log("Guess The Number \n -----------------");
 guess = prompt("Player 1 enter a number to guess : ");
 guess = parseInt(guess);
+
 for(let i=0;i<15;i++)
 {
     console.log("\n");
 }
 
-let player2_input = prompt("Player 2, enter your guess");
-player2_input = parseInt(player2_input);
+for(let i=1;i<=3;i++)
+{   
 
-if(player2_input == guess)
-{
-   console.log("Player 2 won!!!");
+    let player2_input = prompt("Player 2, Attempt -"+i+": ");
+    player2_input = parseInt(player2_input);
+
+    if(player2_input == guess)
+    {
+        console.log("Player 2 won!!!");
+        break;
+    }
+    else 
+    {
+        if(i!=3)
+        console.log("Wrong, try again")
+        else
+        console.log("Player 1 won!")
+       continue;
+    }
+
 }
-else 
-{
-    console.log("Player 1 won!!")
-}
+
