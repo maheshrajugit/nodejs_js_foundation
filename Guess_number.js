ee7dd0ad415bcc54ba3c02bfea6dfeee5d0dbc8d
pavan <pavan@wibesapp.com>
2023-09-29 00:05:27 +0530

    dual player take names added

--- Guess_number.js
+++ Guess_number.js
@@ -1,24 +1,91 @@
 const prompt = require("prompt-sync")();
 
-let guess = 0;
-let attempts= 10;
+let guess = 0;    //global variable declaration
+let attempts= 10;  //global variable
+
+let levels = ["Easy","Hard","Difficult"];
+let attempts_level = [100,10,5];
+
+var players = [];
+
+let opt=0;
+
 console.log("Guess The Number \n -----------------");
 
-console.log("1. Play against computer\n2. 2 Players game");
+select_level();
+choose_players();
 
-let opt = prompt("Choose an Option : ");
 
-if(opt=='1')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function choose_players()
 {
-    guess = Math.floor(Math.random() * 100);
+    console.log("1. Play against computer\n2. 2 Players game");
+
+    opt = prompt("Choose an Option : ");  //global variable - prompt input
+
+    
+
+    if(opt=='1')
+    {
+        guess = Math.floor(Math.random() * 100);
+        take_names(opt);
+        start_game(); 
+        
+
+    }
+    else if(opt == '2')
+    {
+        take_names(opt)
+        player1_start_game();   //function call
+        start_game(); 
+
+    }
 }
-else if(opt == '2')
+
+
+
+
+   //function call
+
+function take_names(option)
 {
-    player1_start_game();   //function call
+   
+    let name = prompt("Enter Player 1 name : ");
+    players.push(name);
+    
+    if(opt=='2')
+    {
+        players.push(prompt("Enter Player 2 name : "))
+    }
+
+    console.log(players);
 }
 
 
-start_game();    //function call
+function select_level()         //ES5 TypeScript function declaration  //arrow function
+{
+    console.log("Choose Level : \n");
+    levels.forEach((item,index)=>{
+        console.log(""+(index+1)+". "+levels[index]);
+    })
+    opt = prompt("select\t: ");
+    attempts = attempts_level[parseInt(opt)-1]
+    
+}
 
 function print_newlines()
 {
@@ -38,25 +105,30 @@ function player1_start_game()
     
 }
 
+
+
 function start_game()    //function defination 
 {
 
-    for(let i = attempts; i>=0; i--)
+    for(let i = attempts; i>0; i--)
     {   
         console.log("you have "+i+" attempts left\n ")
-        let player2_input = prompt("Player 2 : ");
+        let player2_input = prompt(opt=='1'?players[0]+" : ":players[1]+" : ");
         player2_input = parseInt(player2_input);
     
         if(player2_input == guess)
         {
-            console.log("Player 2 won!!!");
+            if(opt=='1')
+            console.log(players[0]+" won!!!");
+            else(opt=='2')
+            console.log(players[1]+" won!!!");
             break;
         }
         else 
         {
             let hint='wrong';
             let d = abs(player2_input - guess);
-            if(i!=0)
+            if(i>1)
             {
                 
                 if(d<=5)
@@ -86,7 +158,18 @@ function start_game()    //function defination
             }
             else
             {
-                console.log("Player 1 won!");
+                var player ="computer";
+                if(opt=='1')
+                {
+                    
+                }
+                else if(opt=='2')
+                {
+                    player = players[0];
+                }
+                console.log(player,"won! :)");
+                console.log(players[1],"lost! :(")
+                console.log("The Guess Number is : ",guess);
 
             }
            continue;
